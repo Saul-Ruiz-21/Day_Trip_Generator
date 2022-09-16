87940c17146fe49bcfb92907dd94cee7065b469c
Saul Ruiz <saulruiz730@gmail.com>
2022-09-16 14:16:52 -0500

    Lists created, on to next steps

---
+++ Main Project.py
@@ -0,0 +1,12 @@
+destinations = ['Japan', 'Russia', 'France', 'England']
+restaurants = ['Steak house', 'Hibachi grill', 'BBQ', 'brewhouse']
+entertainment = ['Arcade', 'Skyzone', 'Zoo', 'Ziplines']
+transportation = ['Bike', 'Bus', 'Car', 'Helecopter']
+
+import random
+
+def random_destination():
+    for words in destinations:
+        print(words)
+
+random_destination()
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # Day_Trip_Generator
 
-hello = 0
-print(hello)
-hh
