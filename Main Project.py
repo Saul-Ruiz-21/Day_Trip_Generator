dc5d55712b84d23405b5fd17b23e60a76691da84
Saul Ruiz <saulruiz730@gmail.com>
2022-09-19 13:35:06 -0500

    finished project

--- Main Project.py
+++ Main Project.py
@@ -7,22 +7,18 @@ import random
 
 def random_destination():
     destination = 'Destination: ' + (random.choice(destinations))
-    print(destination)
     return destination
 
 def random_restaurants():
     restaurant = 'Restaurant: ' + (random.choice(restaurants))
-    print(restaurant)
     return restaurant
 
 def random_entertainment():
     entertainment = 'Entertainment: ' + (random.choice(entertainments))
-    print(entertainment)
     return entertainment
 
 def random_transportation():
     transportation = 'Transportation: ' + (random.choice(transportations))
-    print(transportation)
     return transportation
 
 def trip_lists():
@@ -32,22 +28,42 @@ def trip_lists():
     transportation = random_transportation()
     return [destination, restaurant, entertainment, transportation]
 
+def display_lists(options):
+    for words in options:
+        print(words)
+
+trip = trip_lists()
 
 
 def not_satisfactory_trip():
-    trip = trip_lists()
     question = input('Are you satified with this trip? ')
     while question == 'No':
         re_pick = input("Which option would you like to change? ")
         if re_pick == 'Destination':
-            re_destination = trip + list(random_destination())
-            return re_destination
+            trip[0] = random_destination()
+            display_lists(trip)
+            question = input('Are you satified with this trip? ')
         elif re_pick == 'Restaurant':
-            return trip + list(random_restaurants()) 
+            trip[1] = random_restaurants()
+            display_lists(trip)
+            question = input('Are you satified with this trip? ')
         elif re_pick == 'Entertainment':
-            return trip + list(random_entertainment())
+            trip[2] = random_entertainment()
+            display_lists(trip)
+            question = input('Are you satified with this trip? ')
         elif re_pick == 'Transportation':
-            return trip + list(random_transportation())
+            trip[3] = random_transportation()
+            display_lists(trip)
+        
+            
+        
+
+def satisfactory_trip():
+    print('This is your final trip ')
+    display_lists(trip)        
+
+            
+            
         
 
 
@@ -72,6 +88,8 @@ def not_satisfactory_trip():
 
 
 def run_trip_generator():
+    display_lists(trip)
     not_satisfactory_trip()
-
+    satisfactory_trip()
+    
 run_trip_generator()
